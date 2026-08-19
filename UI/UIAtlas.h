6ed4999704bd13a0d201e885e1e55229df0cd7f3
angelsgabriel810-lab <angelsgabriel810@gmail.com>
2026-08-19 13:10:18 +0200

    Update UIAtlas.h

--- UI/UIAtlas.h
+++ UI/UIAtlas.h
@@ -5,4 +5,14 @@
 
 const Atlas *GetFontAtlas();
 Atlas *GetUIAtlas();
+// --- BOUTONS XBOX ---
+extern int I_XBOX_BTN_A;
+extern int I_XBOX_BTN_B;
+extern int I_XBOX_BTN_X;
+extern int I_XBOX_BTN_Y;
+extern int I_XBOX_LB;
+extern int I_XBOX_LT;
+extern int I_XBOX_RB;
+extern int I_XBOX_RT;
+extern int I_XBOX_GUIDE;
 AtlasData AtlasProvider(Draw::DrawContext *draw, AtlasChoice atlas, float dpiScale, bool invalidate);
